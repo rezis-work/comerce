b22e1baaf2ea5ab15d7718a8ea02630f14f99cc9
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-03-02 09:57:43 +0400

    creating and stylingSearchbar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { BiSearch } from "react-icons/bi";
 
 export default function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <div>
+      <div className=" flex items-center bg-gray-100 p-2 rounded-full max-md:hidden">
+        <button>
+          <BiSearch size={20} className="opacity-50" />
+        </button>
+        <input
+          type="text"
+          className=" outline-none bg-transparent ml-2 caret-blue-500 placeholder:font-light placeholder:text-gray-600 text-[15px] "
+          placeholder="Search"
+          autoComplete="false"
+        />
+      </div>
+    </div>
+  );
 }
